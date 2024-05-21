6c19442af2a76a044ade3fc1f1a8b0fba727c9c1
Miyuranga305 <105289581+Miyuranga305@users.noreply.github.com>
2024-05-21 08:00:38 +0530

    Add files via upload

---
+++ Cafe Management System/CafeAngular/src/main/java/com/cafeManage/CafeAngular/CafeAngularApplication.java
@@ -0,0 +1,13 @@
+package com.cafeManage.CafeAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeAngularApplication.class, args);
+	}
+
+}
---
+++ Cafe Management System/CafeAngular/src/test/java/com/cafeManage/CafeAngular/CafeAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeManage.CafeAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
